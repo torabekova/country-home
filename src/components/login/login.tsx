26624f173768a22a6cdff9ac860b266b7834e35b
torabekova <faridatorabekova6@gmail.com>
2024-12-18 13:58:58 +0500

    experiment

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useForm, Controller } from "react-hook-form";
 import {
   Box,
@@ -21,13 +21,11 @@ import bacgroundimg from "./img/bacgroundimg.png";
 import Header from "pages/Home/Header";
 import Footer from "components/Footer/Footer";
 import Register from "components/register/Register";
-
-import Checkbox from "@mui/material/Checkbox";
 import ForgotPassword from "components/forgot-password/ForgotPAssword";
+import Checkbox from "@mui/material/Checkbox";
 import axios from "axios";
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
-// Yup validatsiya sxemasi
 const schema = yup
   .object({
     email: yup
@@ -51,41 +49,42 @@ const Login: React.FC = () => {
   });
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [isLoginStatus, getIsLoginStatus] = useState(false);
-  const [isPassword, getIsPassword] = useState(false);
+  const [currentView, setCurrentView] = useState<"login" | "forgotPassword" | "register">("login");
   const navigate = useNavigate();
-  const [login, setLogin] = useState<[]>([]);
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  const [isError, setIsError] = useState<boolean>(false)
+  const [isError, setIsError] = useState<boolean>(false);
 
-  
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
   };
-  
+
   const onSubmit = (data: any) => {
     console.log("Login successful:", data);
     axios
-      .post("https://8ff5-95-214-210-65.ngrok-free.app/user/signIn", {
+      .post("https://ce49-95-214-211-5.ngrok-free.app/user/signIn", {
         email,
         password,
       })
-      .then(({status}) => {
+      .then(({ status }) => {
         console.log(status);
-        
         const statusString = status.toString();
-        if (statusString.startsWith('2')) {
+        if (statusString.startsWith("2")) {
           navigate("/Dashboard");
-          return
-        } 
+          return;
+        }
 
         if (statusString.startsWith("4")) {
-          setIsError(true)
+          setIsError(true);
         }
-        
-        // setLogin(response.data);
       })
-      .catch((err)=> {setIsError(true)});
+      .catch((err) => {
+        setIsError(true);
+      });
+  };
+
+  const handleViewChange = (view: "login" | "forgotPassword" | "register") => {
+    setCurrentView(view);
   };
 
   return (
@@ -103,7 +102,7 @@ const Login: React.FC = () => {
           alignItems: "center",
         }}
       >
-        {!isLoginStatus ? (
+        {currentView === "login" && (
           <Box
             sx={{
               maxWidth: "500px",
@@ -144,7 +143,7 @@ const Login: React.FC = () => {
                     }}
                     variant={"outlined"}
                     fullWidth
-                    color={isError ? "error": "primary"}
+                    color={isError ? "error" : "primary"}
                     placeholder="Your Email"
                     InputProps={{
                       startAdornment: (
@@ -266,53 +265,49 @@ const Login: React.FC = () => {
               >
                 Login
               </Button>
-              {!isPassword ? (
-                <div
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "space-between",
-                  }}
-                >
-                  <div style={{ display: "flex", alignItems: "center" }}>
-                    <Checkbox style={{ borderColor: "#C1C7D0" }} {...label} />
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <Checkbox style={{ borderColor: "#C1C7D0" }} {...label} />
 
-                    <Typography
-                      style={{
-                        fontFamily: "Manrope",
-                        fontSize: "14px",
-                        fontWeight: "500",
-                        lineHeight: " 21.7px",
-                        textAlign: "left",
-                        textUnderlinePosition: "from-font",
-                        textDecorationSkipInk: "none",
-                        color: "#666D80",
-                      }}
-                    >
-                      {" "}
-                      Remember me
-                    </Typography>
-                  </div>
-                  <a
-                    onClick={() => getIsPassword(true)}
+                  <Typography
                     style={{
                       fontFamily: "Manrope",
                       fontSize: "14px",
                       fontWeight: "500",
-                      lineHeight: "21.7px",
+                      lineHeight: " 21.7px",
                       textAlign: "left",
                       textUnderlinePosition: "from-font",
                       textDecorationSkipInk: "none",
-                      color: "#C1C7D0",
+                      color: "#666D80",
                     }}
                   >
                     {" "}
-                    Forgot Password ?
-                  </a>
+                    Remember me
+                  </Typography>
                 </div>
-              ) : (
-                <ForgotPassword />
-              )}
+                <a
+                  onClick={() => handleViewChange("forgotPassword")}
+                  style={{
+                    fontFamily: "Manrope",
+                    fontSize: "14px",
+                    fontWeight: "500",
+                    lineHeight: "21.7px",
+                    textAlign: "left",
+                    textUnderlinePosition: "from-font",
+                    textDecorationSkipInk: "none",
+                    color: "#C1C7D0",
+                  }}
+                >
+                  {" "}
+                  Forgot Password ?
+                </a>
+              </div>
             </form>
             <Typography
               className="Sign_up_title"
@@ -322,19 +317,22 @@ const Login: React.FC = () => {
                 color: "#666D80",
               }}
             >
-              Already have an account?{" "}
+              Don't have an account?{" "}
               <a
                 className="sign_up_link"
-                onClick={() => getIsLoginStatus(true)}
+                onClick={() => handleViewChange("register")}
                 style={{ color: "#1BA98F" }}
               >
                 Sign Up
               </a>
             </Typography>
           </Box>
-        ) : (
-          <Register getIsLoginStatus={() => getIsLoginStatus(false)} />
         )}
+
+        {currentView === "forgotPassword" && <ForgotPassword/>}
+        {currentView === "register" && <Register getIsLoginStatus={function (): void {
+          throw new Error("Function not implemented.");
+        } }/>}
       </div>
       <Footer />
     </div>
